e95cb57ee32ced1f7d378642ac79ecfd7aaa06a3
evgenyh <evgeny.h.b@gmail.com>
2024-06-01 13:29:11 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,2 +1,2 @@
-rm -rf trunk/user/www/n56u_ribbon_fixed/bootstrap/img/padavan_logo.png
-cp -rp resources/honeyflower_logo.png trunk/user/www/n56u_ribbon_fixed/bootstrap/img/padavan_logo.png
+rm -rf padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img/padavan_logo.png
+cp -rp resources/honeyflower_logo.png padavan-ng/trunk/user/www/n56u_ribbon_fixed/bootstrap/img/padavan_logo.png
